381d5f7057e74670a6adac1a8953921a8892ce8b
CharlesAcmen <15591542871@163.com>
2026-09-17 16:08:19 +0800

    add ClusteredDeferredResources

--- src/renderers/clustered_deferred.ts
+++ src/renderers/clustered_deferred.ts
@@ -2,9 +2,29 @@ import * as renderer from '../renderer';
 import * as shaders from '../shaders/shaders';
 import { Stage } from '../stage/stage';
 
+interface ClusteredDeferredResources {
+    //producer for G-buffer geo pass
+    gBufferSceneBindGroupLayout: GPUBindGroupLayout;
+    gBufferSceneBindGroup: GPUBindGroup;
+    //consumer for G-buffer fullscreen pass
+    fullscreenBindGroupLayout: GPUBindGroupLayout;
+    fullscreenBindGroup: GPUBindGroup;
+    gBufferPipeline: GPURenderPipeline;
+    fullscreenPipeline: GPURenderPipeline;
+    //g-buffer textures
+    depthTextureView: GPUTextureView;
+    //MRT @location(0)
+    positionTextureView: GPUTextureView;
+    //MRT @location(1)
+    albedoTextureView: GPUTextureView;
+    //MRT @location(2)
+    normalTextureView: GPUTextureView;
+}
+
 export class ClusteredDeferredRenderer extends renderer.Renderer {
     // TODO-3: add layouts, pipelines, textures, etc. needed for Forward+ here
     // you may need extra uniforms such as the camera view matrix and the canvas resolution
+    private readonly resources: ClusteredDeferredResources;
 
     constructor(stage: Stage) {
         super(stage);
